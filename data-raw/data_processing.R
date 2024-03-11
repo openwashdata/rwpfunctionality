be46a32ab55a93ed6afa65e931356ea0e3e11cd0
Götschmann  Margaux <margauxg@ethz.ch>
2024-03-11 14:47:31 +0100

    Export data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,18 @@
+library(tidyverse)
+library(openwashdata)
+# import data -------------------------------------------------------------
+
+data_in <- read_csv("data-raw/cw_Functionality_WP_data.csv") |>
+
+data_out <- data_in
+
+
+
+# export data -------------------------------------------------------------
+
+
+
+
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(data_out, overwrite = TRUE)
